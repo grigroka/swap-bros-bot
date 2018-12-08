67eaa963e9bbf528146c123ccfee5d454b94b09a
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-08 19:54:12 +0200

    reply command

--- commands/main/reply.js
+++ commands/main/reply.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class ReplyCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'reply',
+      group: 'main',
+      memberName: 'reply',
+      description: 'Replies with a Message.',
+      examples: ['reply']
+    });
+  }
+
+  run(msg) {
+    return msg.say("Hi, I'm awake!");
+  }
+};
